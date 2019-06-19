f562550ff72917a8a1af8052790f2d50ec7ea6d3
seydou ba <baseydou95@gmail.com>
2019-06-19 15:14:15 +0000

    creation de la classe client

---
+++ app/src/main/java/com/example/projet_securite/Client.java
@@ -0,0 +1,19 @@
+package com.example.projet_securite;
+
+public class Client {
+
+    public String prenom;
+    public String nom;
+    public String addresse;
+    public String telephone;
+    public String email;
+
+    public Client(String prenom, String nom, String addresse, String telephone, String email) {
+        this.prenom = prenom;
+        this.nom = nom;
+        this.addresse = addresse;
+        this.telephone = telephone;
+        this.email = email;
+    }
+    //public String photo;
+}
