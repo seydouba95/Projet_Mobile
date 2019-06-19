3d197354130d51014e19637a3cae699bd353ea51
seydou ba <baseydou95@gmail.com>
2019-06-19 15:14:40 +0000

    creation de la classe gardien

---
+++ app/src/main/java/com/example/projet_securite/Gardien.java
@@ -0,0 +1,26 @@
+package com.example.projet_securite;
+
+public class Gardien {
+
+    public String nom;
+    public String prenom;
+    public String addresse;
+    public String telephone;
+    public String situation;
+    public String age;
+    public String taille;
+    public String poids;
+   // public String photo;
+
+
+    public Gardien(String nom, String prenom, String addresse, String telephone, String situation, String age, String taille, String poids) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.addresse = addresse;
+        this.telephone = telephone;
+        this.situation = situation;
+        this.age = age;
+        this.taille = taille;
+        this.poids = poids;
+    }
+}
