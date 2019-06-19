ab5e6ad2c8775d86f8f1ece767cecb83da73f114
seydou ba <baseydou95@gmail.com>
2019-06-19 17:53:55 +0000

    point de secours

--- app/src/main/java/com/example/projet_securite/Deposer.java
+++ app/src/main/java/com/example/projet_securite/Deposer.java
@@ -1,10 +1,36 @@
 package com.example.projet_securite;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.DateFormat;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Deposer extends AppCompatActivity {
+
+
+    EditText txt_nom, txt_prenom, txt_date, addresse1, txt_age, txt_taille, txt_poids, tel;
+    Button btn_envoi;
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    Gardien gardien;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +39,48 @@ public class Deposer extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.deposer);
         getSupportActionBar().setTitle("Demande de Recrutement");
+
+        txt_nom = (EditText) findViewById(R.id.txt_nom);
+        txt_prenom = (EditText) findViewById(R.id.txt_prenom);
+        txt_date = (EditText) findViewById(R.id.txt_date);
+        addresse1 = (EditText) findViewById(R.id.addresse1);
+        txt_age = (EditText) findViewById(R.id.txt_age);
+        txt_taille = (EditText) findViewById(R.id.txt_taille);
+        txt_poids = (EditText) findViewById(R.id.txt_poids);
+        btn_envoi = (Button) findViewById(R.id.btn_envoi);
+        tel = (EditText) findViewById(R.id.tel1);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Gardiens");
+
+        btn_envoi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  recruter();
+            }
+        });
+    }
+
+    private void recruter(){
+        String prenom = txt_prenom.getText().toString().trim();
+        String nom = txt_nom.getText().toString().trim();
+        String address = addresse1.getText().toString().trim();
+        String age = txt_age.getText().toString().trim();
+        String taille = txt_taille.getText().toString().trim();
+        String poids = txt_poids.getText().toString().trim();
+        String phone = tel.getText().toString().trim();
+
+
+        if (!TextUtils.isEmpty(prenom) && !TextUtils.isEmpty(nom) && !TextUtils.isEmpty(address) &&
+                !TextUtils.isEmpty(age)  && !TextUtils.isEmpty(taille) && !TextUtils.isEmpty(poids) && !TextUtils.isEmpty(phone)){
+
+            String id= myRef.push().getKey();
+            gardien = new Gardien(id,prenom,nom,address,age,taille,poids,phone);
+            myRef.child(id).setValue(gardien);
+            Toast.makeText(Deposer.this,"envoie reussi",Toast.LENGTH_LONG).show();
+
+        }else{
+            Toast.makeText(Deposer.this,"please entrer  les donnees ",Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/com/example/projet_securite/Gardien.java
+++ app/src/main/java/com/example/projet_securite/Gardien.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 public class Gardien {
 
+  public String id;
     public String nom;
     public String prenom;
     public String addresse;
@@ -11,22 +12,83 @@ public class Gardien {
     public String age;
     public String taille;
     public String poids;
-    public Date date;
+
+   // public Date date;
 
 
 
 
-    public Gardien() {
-    }
-
-    public Gardien(String nom, String prenom, String addresse, String telephone,String age, String taille, String poids, Date date) {
+    public Gardien(String id, String prenom, String nom, String address, String age, String taille, String poids, String phone) {
+        this.id = id;
         this.nom = nom;
         this.prenom = prenom;
-        this.addresse = addresse;
-        this.telephone = telephone;
+        this.addresse = address;
+        this.telephone = phone;
         this.age = age;
         this.taille = taille;
         this.poids = poids;
-        this.date = date;
+
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAddresse() {
+        return addresse;
+    }
+
+    public void setAddresse(String addresse) {
+        this.addresse = addresse;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getTaille() {
+        return taille;
+    }
+
+    public void setTaille(String taille) {
+        this.taille = taille;
+    }
+
+    public String getPoids() {
+        return poids;
+    }
+
+    public void setPoids(String poids) {
+        this.poids = poids;
+    }
+
+
 }
